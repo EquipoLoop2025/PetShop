a05464deb3ae67831e2fbe17fb70c3bbee081435
EquipoLoop2025 <equipoloop2025@gmail.com>
2025-07-02 19:51:53 +0000

    fix flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
 import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
