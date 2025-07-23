85d2994459d4cd5611a8b38028bab114d5b70752
EquipoLoop2025 <equipoloop2025@gmail.com>
2025-07-23 19:13:51 +0000

    Endpoint Login

--- app.py
+++ app.py
@@ -45,8 +45,20 @@ def register():
             return redirect(url_for('register'))
     return render_template('register.html')    
 
-@app.route('/login')
+@app.route('/login', methods = ['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user= supabase.auth.sign_in_with_password({'email': email, 'password' :password})
+            session['user'] = user.user.id
+            session['user_email'] = user.user.email
+            flash('Inicio de Sesion Exitoso', 'success')
+            return redirect(url_for('index'))
+        except Exception as e:
+            flash(f'Error al Inciar Sesion:{e}', 'danger')
+            return redirect(url_for('login'))
     return render_template('login.html')
 
 
